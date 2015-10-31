9c62647525f77a1922497e0943bd28ffa9e4c29b
Toto <graebi@gmail.com>
2015-10-31 17:36:37 +0000

    Initial commit - Login and TABs working fine - no action on TABs so far

---
+++ app/src/main/java/de/tg76/sp3/User.java
@@ -0,0 +1,26 @@
+package de.tg76.sp3;
+
+/**
+ * User class - Creates user and holds attributes of user
+ */
+public class User {
+
+    //Variable to hold value of user
+    String name, username, password, email;
+
+    //Constructor when user register
+    public User (String name, String email,String username, String password){
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    //Constructor when user logs on
+    public User (String username, String password){
+        this.username = username;
+        this.password = password;
+        this.email = "";
+        this.name = "";
+    }
+}
